67cb060b87c6df3b6bf3e6e86fa2876fb989f518
Manuel <mcastagnari@fi.uba.ar>
2026-09-02 18:18:39 -0300

    se mueven todos los archivos relacionados al gateway de pago a una carpeta dentro de forms

--- app/collaborate/card_billing/page.tsx
+++ app/collaborate/card_billing/page.tsx
@@ -3,7 +3,7 @@
 import { onSubmit } from './on_submit';
 import { useEffect } from "react";
 import { loadMercadoPago } from "@mercadopago/sdk-js";
-import PaymentGateway from '@/app/components/forms/paymentGateway';
+import PaymentGateway from '@/app/components/forms/payment_gateway/paymentGateway';
 
 export default function Billing() {
 
