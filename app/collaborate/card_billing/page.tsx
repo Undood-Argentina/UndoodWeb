d9b6ab07c692a259e22a9b235f0dcb6b813e353d
Manuel <mcastagnari@fi.uba.ar>
2026-09-03 00:20:13 -0300

    pagina de donacion unica con pasarela de pago deshabilitada

--- app/collaborate/card_billing/page.tsx
+++ app/collaborate/card_billing/page.tsx
@@ -4,8 +4,10 @@ import { onSubmit } from './on_submit';
 import { useEffect } from "react";
 import { loadMercadoPago } from "@mercadopago/sdk-js";
 import PaymentGateway from '@/app/components/forms/payment_gateway/paymentGateway';
+import { redirect } from 'next/navigation';
 
 export default function Billing() {
+  redirect("/")
 
   return <PaymentGateway />;
 }
